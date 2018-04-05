0329c39c565b2aaa548808a4d1e74b18eb0a65f9
Thortilla <33997787+ThorTila@users.noreply.github.com>
2018-04-05 12:03:50 +0200

    initial

---
+++ server.js
@@ -0,0 +1,33 @@
+var express = require('express'),
+    bodyParser = require('body-parser'),
+    fs = require('fs'),
+    app = express(),
+    stringifyFile,
+    port = 3000;
+
+app.use(bodyParser.json())
+
+app.get('/getNote', function (req, res) {
+    fs.readFile('./test.json', 'utf8', function(err, data) {
+        if (err) throw err;
+        stringifyFile = data;
+        res.send(data);
+    });
+});
+
+app.get('/updateNote/:note', function (req, res) {
+    if (stringifyFile) {
+        stringifyFile += ' ' + req.params.note;
+        fs.writeFile('./test.json', stringifyFile, function(err) {
+            if (err) throw err;
+            console.log('file updated');
+            res.send(stringifyFile);
+        });
+    } else {
+        res.send('Nie stworzono notatki. Najpierw wejdź na localhost:' + port + '/getNote');
+    }
+});
+
+var server = app.listen(port, function() {
+    console.log('Serwer nasłuchuje na http://localhost:' + port);
+});
